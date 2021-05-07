b0396a6390c155449dc9b871afffebfdb4af243f
reedfodge <reedfodge@gmail.com>
2021-05-07 16:14:09 -0400

    Populate museum names in add visitors page

--- public/Visitors/scripts.js
+++ public/Visitors/scripts.js
@@ -1,26 +1,3 @@
-async function getData() {
-  console.log("data request");
-  const result = document.querySelector("#result");
-  const request = await fetch("/api/visitors");
-  const tableData = await request.json();
-  // return tableData;
-
-  tableData.data.forEach((visitor) => {
-    console.log(visitor);
-    const appendItem = document.createElement("tr");
-    // appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
-    appendItem.innerHTML = `
-        <td> ${visitor.visitor_id} </td>
-        <td> ${visitor.visitor_fn} </td>
-        <td> ${visitor.visitor_ln} </td>
-        <td> ${visitor.visitor_phone_num} </td>
-        <td> ${visitor.email} </td>
-        <td> ${visitor.transaction_id} </td>`;
-    result.append(appendItem);
-  });
-}
-window.onload = getData;
-
 /* This is for the nav-bar */
 document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
@@ -45,6 +22,45 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+async function getData() {
+  console.log("data request");
+  const result = document.querySelector("#result");
+  const request = await fetch("/api/visitors");
+  const tableData = await request.json();
+  //For purposes of populating museum name list
+  const requestRole = await fetch('/api/museumStaffRole');
+  const tableDataRole = await requestRole.json();
+  // return tableData;
+
+  //Populate museum name
+  tableDataRole.data.forEach((musNames) => {
+    console.log(musNames);
+    const appendItem = document.createElement('option');
+    appendItem.innerHTML = `
+      <option> ${musNames.Museum_info.museum_name} </option>`;
+    museumIdNames.append(appendItem);
+  });
+
+  tableData.data.forEach((visitor) => {
+    console.log(visitor);
+    const appendItem = document.createElement("tr");
+    // appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
+    appendItem.innerHTML = `
+        <td> ${visitor.visitor_id} </td>
+        <td> ${visitor.visitor_fn} </td>
+        <td> ${visitor.visitor_ln} </td>
+        <td> ${visitor.visitor_phone_num} </td>
+        <td> ${visitor.email} </td>
+        <td> ${visitor.transaction_id} </td>`;
+    result.append(appendItem);
+  });
+}
+window.onload = getData;
+
+const museumIdNames = document.querySelector('#museumIdNames');
+
+
 // class="has-text-light"
 // class="title is-child box has-background-link-dark"
 // class="subtitle has-text-light has-text-weight-bold"
